99584915dfd32fda16dc4409728a50e79d616b47
Devesh Prajapat <devesh.prajapat@phonepe.com>
2021-03-03 21:05:04 +0530

    add qrcode generator

--- OCR/ViewController.swift
+++ OCR/ViewController.swift
@@ -220,3 +220,16 @@ class OCRManager {
     }
     
 }
+
+class QRCodeGenerator {
+    static func generateQR(with url: String) -> CIImage? {
+        let data = url.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+        
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        
+        filter?.setValue(data, forKey: "inputMessage")
+        filter?.setValue("Q", forKey: "inputCorrectionLevel")
+        
+        return filter?.outputImage
+    }
+}
