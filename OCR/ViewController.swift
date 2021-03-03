7e1813d1d915314d65f835a814b5ea01de36fa93
Nikhil Bhownani <nikhil.bhownani@phonepe.com>
2021-03-03 15:10:41 +0530

    Initial Commit

---
+++ OCR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCR
+//
+//  Created by Nikhil Bhownani on 03/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
